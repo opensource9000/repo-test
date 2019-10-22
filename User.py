d02bc5ffd74261cb41949af0fe1fd3735eb5f955
robot401 <gerisen@ieasymail.net>
2019-10-22 08:22:41 -0200

    Adding a new method to the class User into file User.py

--- User.py
+++ User.py
@@ -1,10 +1,13 @@
 class User:
-    def __init__(self, name, lastName):
+    def __init__(self, name, lastName, age):
         self.name = name
         self.lastName = lastName
+        self.age = age
 
 
     def hi(self):
         print(f"Hi, my name is {self.name} {self.lastName}")
 
 
+    def whoami(self):
+        print(f"My name is {self.name} {self.lastName} and I am {self.age}")
